73b44ddbb29c42f639949dfc31160eb97dfaccd4
MMitsui <meil.mitsui@gmail.com>
2020-02-10 12:04:31 +0100

    Edit Californium.properties, Server.java and pom.xml files.

--- src/com/coap/server/main/Server.java
+++ src/com/coap/server/main/Server.java
@@ -1,4 +1,36 @@
 package com.coap.server.main;
 
-public class Server {
+import java.net.SocketException;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+public class Server extends CoapServer {
+
+    public static void main(String[] args) {
+        try {
+            Server server = new Server();
+            server.start();
+        } catch (SocketException e) {
+            System.err.println("Failed to initialize server: " + e.getMessage());
+        }
+    }
+
+    public Server() throws SocketException {
+        add(new PublishResource());
+    }
+
+    class PublishResource extends CoapResource {
+        public PublishResource() {
+            super("publish");
+            getAttributes().setTitle("Publish Resource");
+        }
+        public void handlePOST(CoapExchange exchange) {
+            System.out.println(exchange.getRequestText());
+            exchange.respond("POST_REQUEST_SUCCESS");
+        }
+        public void handleGET(CoapExchange exchange) {
+            exchange.respond("GET_REQUEST_SUCCESS");
+        }
+    }
 }
