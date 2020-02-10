61852089a26d1726c1b0929388023e5534b928d8
MMitsui <meil.mitsui@gmail.com>
2020-02-10 15:39:46 +0100

    Example Works!!

--- src/main/resources/MyResource.java
+++ src/main/resources/MyResource.java
@@ -4,19 +4,27 @@ import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
 public class MyResource extends CoapResource {
+    public MyResource(String name) {
+        super(name);
+    }
+
     @Override
     public void handleGET(CoapExchange exchange) {
         // do something when a GET request is received
         // e.g., reply with a text/plain message
-        exchange.respond("Hello, World!");
+        exchange.respond("Hello, World! from GET");
     }
 
     @Override
     public void handlePOST(CoapExchange exchange) {
-        if (exchange.getRequestOptions()....) {
+        String txt = "This is ";
+        //if (exchange.getRequestOptions()....) {
+        if(true) {
             // do something specific to the request options
+            txt += "TRUE";
+            System.out.printf("IF is TRUE!!\n");
         }
         // reply with response code only (shortcut)
-        exchange.respond(CREATED);
+        exchange.respond(txt + " CREATED");
     }
 }
